6124897e742e075d90e702c83d40544f7077d2bb
KHidoyatov7 <muhammadaminhidoyatov6@gmail.com>
2024-02-10 21:00:22 +0500

    I do an exam

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -5,11 +5,3 @@ interface ContainerProps {
   style?: React.CSSProperties; // For inline styles
   id?: string; // For a container ID
 }
-
-
-const Container: React.FC<ContainerProps> = ({ children, className }) => {
-    return <div className={container}>{children}</div>;
-  };
-
-  
-  export default Container
