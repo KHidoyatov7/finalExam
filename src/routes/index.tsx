ee03bc36bd028ebed9198a81404059af75a960ee
KHidoyatov7 <muhammadaminhidoyatov6@gmail.com>
2024-02-13 20:23:51 +0500

    I do exam

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,14 @@
 import {Routes, Route} from 'react-router-dom'
 import { FontsDisplay } from '../routes/home'
 import SingleFont from '../routes/singleFont'
+import { Font } from '../types'
 const RouteController = () => {
   return (
     <div>
         <Routes>
         <Route path='/' element={<FontsDisplay/>}/>
           <Route path='/singlefont' element={<SingleFont onBack={function (): void {
-            throw new Error('Function not implemented.')
-          } } font={{
-            family: '',
-            category: ''
-          }}/>}/>
+            throw new Error('Function not implemented.')}} font={{} as Font}/>}/>
         </Routes>
     </div>
   )
